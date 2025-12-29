ad50dd1ac666eba3f4b88302fc6dda732b968f06
Tawhid Hasan <tawhidhasan88@gmail.com>
2025-12-29 11:05:15 +0600

    Fix2

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig: NextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
+
+
+    //Tawhid
     ignoreDuringBuilds: true,
   },
 };
